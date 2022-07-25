98b389252c7c0da31a44ae89cd5c894127c244e0
Subramanya Swaroop S P <84366195+subramanyaswaroop@users.noreply.github.com>
2022-07-25 18:51:55 +0530

    Add files via upload

---
+++ pattern_24/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("enter a number");
+int n=int.Parse(Console.ReadLine());
+int val = 0;
+int value = 1;
+
+Console.WriteLine("");
+Console.WriteLine("");for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("");
+    for (int j = 0; j <=i; j++)
+    {
+        val += 1;
+        Console.Write(val);
+    }
+    val = 0;
+}
+for (int i = 1; i < n; i++)
+{
+    Console.WriteLine("");
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write(i);
+    }
+}
+Console.WriteLine("");
+Console.WriteLine("");
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("");
+    for (int j = 0; j <= i; j++)
+    {
+        val += 1;
+        Console.Write(val);
+    }
+    
+}
+Console.WriteLine("");
+
