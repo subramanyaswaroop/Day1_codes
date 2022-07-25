d7e7a24809b329fc147c41ba88cc931503331320
Subramanya Swaroop S P <84366195+subramanyaswaroop@users.noreply.github.com>
2022-07-25 17:39:05 +0530

    Add files via upload

---
+++ reverse_of_number/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("enter a number");
+int n= int.Parse(Console.ReadLine());
+int rev=0;
+int rem;
+int i = 0;
+while (n!=0)
+{
+    rem = n % 10;
+    rev = rev * 10 + rem;
+    n = n / 10;
+
+}
+Console.WriteLine("Reverse ->" + rev);
---
+++ sum_of_all_odd/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter value of n");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i < n; i=i+2)
+{
+    sum += i;
+}
+Console.WriteLine($"sum is {sum}");
