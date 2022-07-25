8d5c4bc4529afeab6d184d348291e6e5565ea2b3
Subramanya Swaroop S P <84366195+subramanyaswaroop@users.noreply.github.com>
2022-07-25 17:57:56 +0530

    Add files via upload

---
+++ wholenum_fraction_seperation/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Enter a number");
+float n= float.Parse(Console.ReadLine());
+int x = (int)n;
+float fraction = n - x;
+Console.WriteLine("Whole number is ->"+x);
+Console.WriteLine("Fractional value is ->"+fraction);
