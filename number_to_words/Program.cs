e4c5c5977674a1c136eb349ac75b906493b15ee8
Subramanya Swaroop S P <84366195+subramanyaswaroop@users.noreply.github.com>
2022-07-26 16:32:10 +0530

    Add files via upload

---
+++ number_to_words/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Enter a number");
+string num =Console.ReadLine();
+for (int i = 0; i < num.Length; i++)
+{
+    if (num[i]=='0')
+    {
+        Console.Write("Zero");
+    }
+    else if (num[i] == '1')
+    {
+        Console.Write("One");
+    }
+     else if (num[i] == '2')
+    {
+        Console.Write("Two");
+    }
+    else if (num[i] == '3')
+    {
+        Console.Write("Three");
+    }
+    else if (num[i] == '4')
+    {
+        Console.Write("Four");
+    }
+    else if (num[i] == '5')
+    {
+        Console.Write("Five");
+    }
+    else if (num[i] == '6')
+    {
+        Console.Write("Six");
+    }
+    else if (num[i] == '7')
+    {
+        Console.Write("Seven");
+    }
+    else if (num[i] == '8')
+    {
+        Console.Write("Eight");
+    }
+    else if (num[i] == '9')
+    {
+        Console.Write("Nine");
+    }
+ 
+
+}
